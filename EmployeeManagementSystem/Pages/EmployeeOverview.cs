a5f13df9b53d25e65b6cecb3cf8b58f066a15b9c
ammarmangala <ammar.mangala@gmail.com>
2022-10-28 19:20:45 +0200

    create employee table in employeeoverview.razor

--- EmployeeManagementSystem/Pages/EmployeeOverview.cs
+++ EmployeeManagementSystem/Pages/EmployeeOverview.cs
@@ -1,6 +1,14 @@
+using EmployeeManagementSystem.Models;
+using EmployeeManagementSystem.Shared.Domain;
+
 namespace EmployeeManagementSystem.Pages;
 
 public partial class EmployeeOverview
 {
-    
-}
+    public List<Employee>? Employees { get; set; } = default;
+
+    protected override void OnInitialized()
+    {
+        Employees = MockDataService.Employees;
+    }
+} 
