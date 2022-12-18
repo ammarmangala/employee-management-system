de49760eaaefb63f8e681b14faa5043445f954e3
ammarmangala <ammar.mangala@gmail.com>
2022-12-18 01:53:12 +0100

    bug fixes

--- EmployeeManagementSystem/Pages/EmployeeOverview.cs
+++ EmployeeManagementSystem/Pages/EmployeeOverview.cs
@@ -4,7 +4,7 @@ using EmployeeManagementSystem.Shared.Domain;
 namespace EmployeeManagementSystem.Pages;
 
 public partial class EmployeeOverview
-{
+{ 
     public List<Employee>? Employees { get; set; } = default;
 
     protected override void OnInitialized()
