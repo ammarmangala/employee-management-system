a109ab718704800b0b13da4e554e0960e9a85d9a
ammarmangala <ammar.mangala@gmail.com>
2023-01-07 15:46:43 +0100

    updated sdk version

--- EmployeeManagementSystem/Pages/EmployeeOverview.cs
+++ EmployeeManagementSystem/Pages/EmployeeOverview.cs
@@ -4,11 +4,12 @@ using EmployeeManagementSystem.Shared.Domain;
 namespace EmployeeManagementSystem.Pages;
 
 public partial class EmployeeOverview
-{ 
+{
     public List<Employee>? Employees { get; set; } = default;
 
     protected override void OnInitialized()
     {
+        // Get the list of employees from the MockDataService
         Employees = MockDataService.Employees;
     }
-} 
+}
