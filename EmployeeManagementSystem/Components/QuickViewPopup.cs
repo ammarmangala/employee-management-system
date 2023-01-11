8e6a54c02e7c57db15b98ffcd0eb430400a6216e
ammarmangala <ammar.mangala@gmail.com>
2023-01-11 15:40:31 +0100

    review quickreviewpopup razor component

--- EmployeeManagementSystem/Components/QuickViewPopup.cs
+++ EmployeeManagementSystem/Components/QuickViewPopup.cs
@@ -1,6 +1,12 @@
+using EmployeeManagementSystem.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
 namespace EmployeeManagementSystem.Components;
 
 public partial class QuickViewPopup
 {
-    
+    [Parameter]
+    public Employee? Employee { get; set; }
+
+    private Employee? _employee;
 }
