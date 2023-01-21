449f4445bc28e5016da9452917d72cb59040a1af
ammarmangala <ammar.mangala@gmail.com>
2023-01-21 18:36:44 +0100

    create EmployeeEdit component & class

---
+++ EmployeeManagementSystem/Pages/EmployeeEdit.cs
@@ -0,0 +1,17 @@
+using EmployeeManagementSystem.Services;
+using EmployeeManagementSystem.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagementSystem.Pages;
+
+public partial class EmployeeEdit
+{
+    [Inject]
+    public IEmployeeDataService? EmployeeDataService { get; set; }
+    
+    [Parameter]
+    public string? EmployeeId { get; set; }
+
+    public Employee Employee { get; set; } = new Employee();
+    
+}
