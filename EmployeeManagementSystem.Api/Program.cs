679aec57d21f229b0f62f5001146384fe79008f9
Ammar Mangala Kitungwa <ammar-macbookpro-14@Ammars-MacBook-Pro.local>
2023-03-17 08:03:27 +0100

    update db ef

--- EmployeeManagementSystem.Api/Program.cs
+++ EmployeeManagementSystem.Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<AppDbContext>(options => {
-    options.UseSqlServer(
+    options.UseNpgsql(
         builder.Configuration["ConnectionStrings:DefaultConnection"]);
 });
 
